15cc61dc06520eae49be17f9f1e2a4fa2b33e151
D-U-D-U <claudioecbarros@gmail.com>
2025-05-28 21:32:00 -0300

    Aula de TypeScript

---
+++ AulasTS/src/index.ts
@@ -0,0 +1,12 @@
+import { funcionario } from './entidades/funcionario';
+
+
+
+var joao = new funcionario("123.456.789-00", "João da Silva", "M", new Date("1990-01-01"), "joao.b@gmail.com", 3000)
+
+console.log(joao)
+
+
+
+
+console.log("Hello Word! Teste @ Teste")
